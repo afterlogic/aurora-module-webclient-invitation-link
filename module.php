186b2128e0c31f7e002fcf1780c73e7966f07ba2
sash04ek <a.ovcharov@gmail.com>
2016-08-12 11:05:20 +0300

    generate real magic-link

--- module.php
+++ module.php
@@ -2,6 +2,28 @@
 
 class MagicLinkWebclientModule extends AApiModule
 {
+	protected $oMinModuleDecorator;
+	
+	protected $aRequireModules = array(
+		'Min'
+	); 
+	
+	/**
+	* Returns Min module decorator.
+	* 
+	* @return \CApiModuleDecorator
+	*/
+	private function getMinModuleDecorator()
+	{
+		if ($this->oMinModuleDecorator === null)
+		{
+			$this->oMinModuleDecorator = \CApi::GetModuleDecorator('Min');
+		}
+		
+		return $this->oMinModuleDecorator;
+	}
+		
+	
 	public function init()
 	{
 		$this->includeTemplate('AdminPanelWebclient_EditUserView', 'Edit-User-After', 'templates/MagicLinkView.html', $this->sName);
@@ -9,12 +31,32 @@ class MagicLinkWebclientModule extends AApiModule
 	
 	public function GetMagicLink($UserId)
 	{
+		$mHash = '';
+		$oMin = $this->getMinModuleDecorator();
+		if ($oMin)
+		{
+			$sMinId = implode('|', array($UserId, md5($UserId)));
+			$mHash = $oMin->GetMinById($sMinId);
+
+			if (!$mHash)
+			{
+				$mHash = $oMin->CreateMin($sMinId, array($UserId));
+			}
+			else 
+			{
+				if (isset($mHash['__hash__']))
+				{
+					$mHash = $mHash['__hash__'];
+				}
+			}
+		}
+		
 		$oAuthenticatedUser = \CApi::getAuthenticatedUser();
 		if (empty($oAuthenticatedUser) || $oAuthenticatedUser->Role !== \EUserRole::SuperAdmin)
 		{
 			return '';
 		}
 		
-		return \api_Utils::GetAppUrl() . '?magic-link=' . md5($UserId);
+		return \api_Utils::GetAppUrl() . '?magic-link=' . $mHash;
 	}
 }

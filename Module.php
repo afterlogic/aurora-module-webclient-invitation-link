f64bd8a79858823b7e1dea7ace917bd66c7c048d
sash04ek <a.ovcharov@gmail.com>
2017-03-07 11:22:57 +0300

    added

--- Module.php
+++ Module.php
@@ -20,7 +20,7 @@
 
 namespace Aurora\Modules\InvitationLinkWebclient;
 
-class Module extends \Aurora\System\Module\AbstractModule
+class Module extends \Aurora\System\Module\AbstractWebclientModule
 {
 	protected $oMinModuleDecorator;
 	

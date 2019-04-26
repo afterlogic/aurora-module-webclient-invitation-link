2fe92ab75de21d2e96dd7cb1578470de2dfb56e9
nadine-shi <nadine@afterlogic.com>
2019-04-26 14:44:01 +0300

    License and other texts improving

--- Module.php
+++ Module.php
@@ -8,6 +8,8 @@
 namespace Aurora\Modules\InvitationLinkWebclient;
 
 /**
+ * Creates invitation link upon creating user in admin panel, and allows registering new user account with this link.
+ * 
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
  * @copyright Copyright (c) 2019, Afterlogic Corp.

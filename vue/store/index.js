f5db8914723b7b42d1eb96fdb6371de0d8f3a987
nadine-shi <nadinshi@gmail.com>
2023-05-02 15:17:01 +0300

    Switched to Quasar2/VueJs3 (doBeforeRouteLeave method and Vue.set)

--- vue/store/index.js
+++ vue/store/index.js
@@ -1,5 +1,3 @@
-import Vue from 'vue'
-
 import typesUtils from 'src/utils/types'
 import webApi from 'src/utils/web-api'
 
@@ -9,32 +7,34 @@ export default {
     invitationLinks: {},
   },
   mutations: {
-    setInvitationLink (state, { tenantId, userId, publicId, hash }) {
-      Vue.set(state.invitationLinks, userId, { tenantId, userId, publicId, hash })
+    setInvitationLink(state, { tenantId, userId, publicId, hash }) {
+      state.invitationLinks[userId] = { tenantId, userId, publicId, hash }
     },
   },
   actions: {
-    requestInvitationLink ({ state, commit }, { tenantId, userId, publicId }) {
+    requestInvitationLink({ state, commit }, { tenantId, userId, publicId }) {
       if (!state.invitationLinks[userId]) {
         const parameters = {
           TenantId: tenantId,
           UserId: userId,
           Email: publicId, // this parameter will be used im manager.js for just created users, server doesn't expect it
         }
-        webApi.sendRequest({
-          moduleName: 'InvitationLinkWebclient',
-          methodName: 'GetInvitationLinkHash',
-          parameters,
-        }).then(result => {
-          if (typesUtils.isNonEmptyString(result)) {
-            commit('setInvitationLink', { tenantId, userId, publicId, hash: result })
-          }
-        })
+        webApi
+          .sendRequest({
+            moduleName: 'InvitationLinkWebclient',
+            methodName: 'GetInvitationLinkHash',
+            parameters,
+          })
+          .then((result) => {
+            if (typesUtils.isNonEmptyString(result)) {
+              commit('setInvitationLink', { tenantId, userId, publicId, hash: result })
+            }
+          })
       }
     },
   },
   getters: {
-    getInvitationLinks (state) {
+    getInvitationLinks(state) {
       return state.invitationLinks
     },
   },
